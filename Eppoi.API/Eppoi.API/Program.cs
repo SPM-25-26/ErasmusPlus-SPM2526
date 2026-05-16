3927f096adfa558b7fe2a6fd79b6da51b3118049
Andrea Malloni <malloni.andrea02@gmail.com>
2026-05-16 16:20:25 +0200

    Backend project init, Swagger and Serilog setup

---
+++ Eppoi.API/Eppoi.API/Program.cs
@@ -0,0 +1,65 @@
+using Serilog;
+using Serilog.Events;
+
+namespace Eppoi.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                .WriteTo.Logger(l => l
+                    .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Information)
+                    .WriteTo.File("Logs/info-.txt", rollingInterval: RollingInterval.Day))
+                .WriteTo.Logger(l => l
+                    .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning)
+                    .WriteTo.File("Logs/warning-.txt", rollingInterval: RollingInterval.Day))
+                .WriteTo.Logger(l => l
+                    .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error)
+                    .WriteTo.File("Logs/error-.txt", rollingInterval: RollingInterval.Day))
+                .WriteTo.Logger(l => l
+                    .Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Fatal)
+                    .WriteTo.File("Logs/fatal-.txt", rollingInterval: RollingInterval.Day))
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web application");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog();
+
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseAuthorization();
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
