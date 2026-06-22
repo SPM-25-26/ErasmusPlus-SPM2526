8aea221d2ec274fd2b819769e6ba1af9562fefec
Andrea Malloni <malloni.andrea02@gmail.com>
2026-06-22 16:17:39 +0200

    Added DTOs and Controllers for Organizations and Sleep/accomodations GET API

---
+++ Eppoi.API/Eppoi.API/DTOs/SleepDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Eppoi.API.DTOs
+{
+    public class SleepSummaryDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string? OfficialName { get; set; }
+        public string? Typology { get; set; } 
+        public string? Classification { get; set; }
+        public string? PrimaryImagePath { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class SleepDetailDto : SleepSummaryDto
+    {
+        public string? Description { get; set; }
+        public string? ShortAddress { get; set; }
+        public List<string>? RoomTypologies { get; set; } 
+        public string? Email { get; set; }
+        public string? Telephone { get; set; }
+        public string? Website { get; set; }
+        public string? Facebook { get; set; }
+        public string? Instagram { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public List<string>? Gallery { get; set; }
+        public List<string>? VirtualTours { get; set; }
+
+        public OpeningHourDto? OpeningHours { get; set; }
+        public List<OfferDto> Offers { get; set; } = new();
+    }
+}
---
+++ Eppoi.API/Eppoi.API/Mappers/SleepMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Eppoi.API.DTOs;
+using Eppoi.API.Entities;
+
+namespace Eppoi.API.Mappers
+{
+    public class SleepMappingProfile : Profile
+    {
+        public SleepMappingProfile()
+        {
+            CreateMap<PoisSleep, SleepSummaryDto>()
+                .ForMember(dest => dest.OfficialName, opt => opt.MapFrom(src => src.IdNavigation.OfficialName))
+                .ForMember(dest => dest.PrimaryImagePath, opt => opt.MapFrom(src => src.IdNavigation.PrimaryImagePath))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.IdNavigation.Address));
+
+            CreateMap<PoisSleep, SleepDetailDto>()
+                .IncludeBase<PoisSleep, SleepSummaryDto>()
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.IdNavigation.Description))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.IdNavigation.Email))
+                .ForMember(dest => dest.Telephone, opt => opt.MapFrom(src => src.IdNavigation.Telephone))
+                .ForMember(dest => dest.Website, opt => opt.MapFrom(src => src.IdNavigation.Website))
+                .ForMember(dest => dest.Facebook, opt => opt.MapFrom(src => src.IdNavigation.Facebook))
+                .ForMember(dest => dest.Instagram, opt => opt.MapFrom(src => src.IdNavigation.Instagram))
+                .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.IdNavigation.Latitude))
+                .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.IdNavigation.Longitude))
+                .ForMember(dest => dest.Gallery, opt => opt.MapFrom(src => src.IdNavigation.Gallery))
+                .ForMember(dest => dest.VirtualTours, opt => opt.MapFrom(src => src.IdNavigation.VirtualTours));
+        }
+    }
+}
